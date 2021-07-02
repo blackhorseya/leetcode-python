9e56e931c2920db83844da12486a6734b590d11f
Sean Zheng <blackhorseya@gmail.com>
2021-07-02 08:21:25 +0800

    refactor: modified 0035 problem

--- 0035/solution.py
+++ 0035/solution.py
@@ -3,12 +3,17 @@ from typing import List
 
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        ret = 0
+        left, right = 0, len(nums) - 1
 
-        for i, num in enumerate(nums):
-            if num < target:
-                ret = i + 1
-            else:
-                break
+        while left <= right:
+            mid = (left + right) // 2
+            val = nums[mid]
 
-        return ret
+            if target == val:
+                return mid
+            elif target > val:
+                left = mid + 1
+            elif target < val:
+                right = mid - 1
+
+        return left
